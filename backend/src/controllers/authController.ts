69539f87fdb48fc8212148c1214f40a0e4bb37e1
Prema Sagar Bontula <137033028+premasagarbontula@users.noreply.github.com>
2026-06-19 19:05:21 +0530

    Setup full stack project structure and backend authentication foundation

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+
+import User from "../models/User";
+import generateToken from "../utils/generateToken";
+
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({
+      email,
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = generateToken(user._id.toString());
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({
+      message: "User registered",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
---
+++ backend/src/server.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db";
+
+import authRoutes from "./routes/authRoutes";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
+//routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Travel Planner API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
